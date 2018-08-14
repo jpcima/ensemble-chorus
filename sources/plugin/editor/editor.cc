4f85dfc7a7388396add0ad31fae55e185fdda72d
JP Cimalando <jpcima@users.noreply.github.com>
2018-08-14 13:12:25 +0200

    plugin ui: knob parameter handling

--- sources/plugin/editor/editor.cc
+++ sources/plugin/editor/editor.cc
@@ -5,8 +5,11 @@
 
 #include "plugin/editor/editor.h"
 #include "plugin/editor/widgets/knob.h"
+#include "ensemble_chorus.h"
 
 struct Chorus_UI::Impl : public Knob::Callback {
+    Chorus_UI *const Q = nullptr;
+
     Color bg_{0xc0, 0xc0, 0xc0};
     std::unique_ptr<Knob> kn_mod_range_;
     std::unique_ptr<Knob> kn_slow_rate_;
@@ -16,6 +19,9 @@ struct Chorus_UI::Impl : public Knob::Callback {
     std::unique_ptr<Knob> kn_lpf_cutoff_;
     std::unique_ptr<Knob> kn_lpf_q_;
 
+    explicit Impl(Chorus_UI *q)
+        : Q(q) {}
+
     void knobDragStarted(Knob *knob) override {}
     void knobDragFinished(Knob *knob) override {}
     void knobValueChanged(Knob *knob, float value) override;
@@ -23,7 +29,7 @@ struct Chorus_UI::Impl : public Knob::Callback {
 
 Chorus_UI::Chorus_UI()
     : UI(625, 280),
-      P(new Impl)
+      P(new Impl(this))
 {
     Window &win = getParentWindow();
 
@@ -47,9 +53,155 @@ Chorus_UI::Chorus_UI()
 
 void Chorus_UI::parameterChanged(uint32_t index, float value)
 {
-    // TODO
-    
-    
+    switch (index) {
+    case ECP_BYPASS:
+        // TODO
+        break;
+    case ECP_CHANNEL_LAYOUT:
+        // TODO
+        break;
+    case ECP_DELAY:
+        // TODO
+        break;
+    case ECP_NSTAGES:
+        // TODO
+        break;
+    case ECP_MOD_RANGE:
+        P->kn_mod_range_->setValue(value);
+        break;
+    case ECP_SLOW_RATE:
+        P->kn_slow_rate_->setValue(value);
+        break;
+    case ECP_SLOW_WAVE:
+        // TODO
+        break;
+    case ECP_SLOW_RAND:
+        P->kn_slow_rand_->setValue(value);
+        break;
+    case ECP_FAST_RATE:
+        P->kn_fast_rate_->setValue(value);
+        break;
+    case ECP_FAST_WAVE:
+        // TODO
+        break;
+    case ECP_FAST_RAND:
+        P->kn_fast_rand_->setValue(value);
+        break;
+    case ECP_LPF_CUTOFF:
+        P->kn_lpf_cutoff_->setValue(value);
+        break;
+    case ECP_LPF_Q:
+        P->kn_lpf_q_->setValue(value);
+        break;
+    case ECP_GAIN_IN:
+        // TODO
+        break;
+    case ECP_GAIN_OUT:
+        // TODO
+        break;
+    case ECP_MIX_DRY:
+        // TODO
+        break;
+    case ECP_MIX_WET:
+        // TODO
+        break;
+
+    case ECP_ENABLE1:
+        // TODO
+        break;
+    case ECP_PHASE1:
+        // TODO
+        break;
+    case ECP_DEPTH1:
+        // TODO
+        break;
+    case ECP_ROUTE_L1:
+        // TODO
+        break;
+    case ECP_ROUTE_R1:
+        // TODO
+        break;
+
+    case ECP_ENABLE2:
+        // TODO
+        break;
+    case ECP_PHASE2:
+        // TODO
+        break;
+    case ECP_DEPTH2:
+        // TODO
+        break;
+    case ECP_ROUTE_L2:
+        // TODO
+        break;
+    case ECP_ROUTE_R2:
+        // TODO
+        break;
+
+    case ECP_ENABLE3:
+        // TODO
+        break;
+    case ECP_PHASE3:
+        // TODO
+        break;
+    case ECP_DEPTH3:
+        // TODO
+        break;
+    case ECP_ROUTE_L3:
+        // TODO
+        break;
+    case ECP_ROUTE_R3:
+        // TODO
+        break;
+
+    case ECP_ENABLE4:
+        // TODO
+        break;
+    case ECP_PHASE4:
+        // TODO
+        break;
+    case ECP_DEPTH4:
+        // TODO
+        break;
+    case ECP_ROUTE_L4:
+        // TODO
+        break;
+    case ECP_ROUTE_R4:
+        // TODO
+        break;
+
+    case ECP_ENABLE5:
+        // TODO
+        break;
+    case ECP_PHASE5:
+        // TODO
+        break;
+    case ECP_DEPTH5:
+        // TODO
+        break;
+    case ECP_ROUTE_L5:
+        // TODO
+        break;
+    case ECP_ROUTE_R5:
+        // TODO
+        break;
+
+    case ECP_ENABLE6:
+        // TODO
+        break;
+    case ECP_PHASE6:
+        // TODO
+        break;
+    case ECP_DEPTH6:
+        // TODO
+        break;
+    case ECP_ROUTE_L6:
+        // TODO
+        break;
+    case ECP_ROUTE_R6:
+        // TODO
+        break;
+    }
 }
 
 void Chorus_UI::onNanoDisplay()
@@ -65,28 +217,26 @@ void Chorus_UI::onNanoDisplay()
 
 void Chorus_UI::Impl::knobValueChanged(Knob *knob, float value)
 {
-    // TODO
-
     if (knob == kn_mod_range_.get()) {
-        
+        Q->setParameterValue(ECP_MOD_RANGE, value);
     }
     else if (knob == kn_slow_rate_.get()) {
-        
+        Q->setParameterValue(ECP_SLOW_RATE, value);
     }
     else if (knob == kn_fast_rate_.get()) {
-        
+        Q->setParameterValue(ECP_FAST_RATE, value);
     }
     else if (knob == kn_slow_rand_.get()) {
-        
+        Q->setParameterValue(ECP_SLOW_RAND, value);
     }
     else if (knob == kn_fast_rand_.get()) {
-        
+        Q->setParameterValue(ECP_FAST_RAND, value);
     }
     else if (knob == kn_lpf_cutoff_.get()) {
-        
+        Q->setParameterValue(ECP_LPF_CUTOFF, value);
     }
     else if (knob == kn_lpf_q_.get()) {
-        
+        Q->setParameterValue(ECP_LPF_Q, value);
     }
 }
 
